a4743a827dfb82fe60861fe87547148ab1a79ca3
franchescafernanda <franchescafer26@gmail.com>
2024-02-11 01:10:30 -0300

    Add: information in App.jsx

--- desafio/src/App.jsx
+++ desafio/src/App.jsx
@@ -13,7 +13,8 @@ function App() {
     <>
       <Header titulo = "Adopta un perrito"/>
       <Card tarjet/>
-      <Footer description ="Explora nuestra galería de adopción de perros para encontrar a tu compañero perfecto."/>
+      <Footer description ="Explora nuestra galería de adopción de perros para encontrar a tu compañero perfecto.
+      Tenemos una variedad de perros con diferentes personalidades y tamaños, todos en busca de un hogar cálido y amoroso."/>
       
     </>
   )
