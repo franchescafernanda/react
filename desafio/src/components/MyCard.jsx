dd71b7d4c86aaa2ae5953695b004daa99a344434
franchescafernanda <franchescafer26@gmail.com>
2024-02-11 14:40:00 -0300

    Add: App.jsx

--- desafio/src/components/MyCard.jsx
+++ desafio/src/components/MyCard.jsx
@@ -8,7 +8,7 @@ const MyCard = (props) => {
   
       return (
         <>
-        <div className="card">
+        <div className="tarjet">
           <Card style = {{ whidth: '18rem'}}>
             <Tags colorBg={props.colorBg} textBg ={props.props.textBg}/>
             <Card.img variant="top" src={props.img}/>
@@ -26,36 +26,3 @@ const MyCard = (props) => {
     }
     
     export default MyCard
-
-
-
-
-
-
-
-          {/* debo traspasarlo a Bootstrap
-          
-          <Card
-              img="https://images.pexels.com/photos/803766/pexels-photo-803766.jpeg?auto=compress&cs=tinysrgb&w=300"
-              name="Bartolo"
-              information="Lleno de energía y listo para jugar. !Dale a Bartolo el hogar amoroso que se merece¡"
-              btn="Husky"
-          />
-          <Card
-              img="https://images.pexels.com/photos/16353650/pexels-photo-16353650/free-photo-of-jardin-animal-perro-mascota.jpeg?auto=compress&cs=tinysrgb&w=300"
-              name="Gohan"
-              information=""
-              btn="Shar Pei"
-          />
-          <Card
-              img="https://images.pexels.com/photos/373467/pexels-photo-373467.jpeg?auto=compress&cs=tinysrgb&w=300"
-              name="Chuchu"
-              information="Es jugueton, amigable y cariñoso con los niños. !Haz de Messi parte de tu familia hoy¡"
-              btn="Poddle"
-          />
-          <Card
-              img="https://images.pexels.com/photos/3764319/pexels-photo-3764319.jpeg?auto=compress&cs=tinysrgb&w=300"
-              name="Princesa"
-              information="Es una compañera leal y cariñosa que adora los mimos y los abrazos !Ayuda a Princesa a encontrar su final feliz¡"
-              btn="Beagle"
-          /> */}
